4aa568899aa4c902979adbba82e4d899807e1d74
rafaelvantin <rafael.sartori.vantin@gmail.com>
2020-10-15 13:35:16 -0300

    fixed server to app

--- src/index.js
+++ src/index.js
@@ -17,4 +17,4 @@ require("./controllers/AuthController.js")(app);
 const server = require("http").createServer(app);
 require("./config/socket.js")(server);
 
-server.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
